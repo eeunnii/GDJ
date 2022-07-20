0bd0c9621b92b782d20ac303602340d3fd2d9708
eeunnii <owo667@naver.com>
2022-07-20 17:22:34 +0900

    2022-07-20

---
+++ javastudy/02_CONTROL/src/Ex01_if.java
@@ -0,0 +1,35 @@
+
+public class Ex01_if {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		// if 문
+		// if(조건) {
+		//			실행문_조건만족하는경우 실행함, 대괄호 지워도 출력됨. 단 실행문 하나일 때 
+		//				}
+		
+		int score = 50;
+		
+		if (score >= 60)  {
+			System.out.println("합격");
+			System.out.println("축하합니다");
+							}
+		
+		if(score<60) {
+			System.out.println("불합격");
+						}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
